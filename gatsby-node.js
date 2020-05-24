2d9ee9562718656141e16453580dc8edc9bbadba
David <sanderdj90@gmail.com>
2020-05-23 21:59:53 -0600

    v1.1.0 remove missing images, repalce w missingNO

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,22 @@
 const { results } = require("pokemons");
-const uniquePokemons = Array.from(new Set(results.map(a => a.name)))
- .map(name => {
-   return results.find(a => a.name === name)
- })
+const noImgPokemon = require("./noImgPokemonarr.json");
+
+const uniquePokemons = Array.from(new Set(results.map(a => a.name))).map(
+  name => {
+    return results.find(a => a.name === name);
+  }
+);
+
+const cleaned = uniquePokemons.map(pokemon => {
+  if (noImgPokemon.includes(pokemon.sprites.normal) || pokemon.name.includes("Nidoran")) {
+    pokemon.sprites.normal =
+      "https://wiki.p-insurgence.com/images/0/09/722.png";
+    return pokemon;
+  } else {
+    return pokemon;
+  }
+});
+
 exports.sourceNodes = async ({
   actions,
   createNodeId,
@@ -10,7 +24,7 @@ exports.sourceNodes = async ({
 }) => {
   try {
     // Map over the results array, calling action.createNode on each item in the array
-   uniquePokemons.forEach((pokemon, i) => {
+    cleaned.forEach((pokemon, i) => {
       const node = {
         ...pokemon, // We copy all of the properties from the pokemon object
         id: createNodeId(i), // Needs to be unique
